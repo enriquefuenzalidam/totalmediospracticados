9297e9671b3befb77364aeb5fa84ba47200a3653
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-06-18 21:54:43 -0400

    commit tre

--- app/page.jsx
+++ app/page.jsx
@@ -30,7 +30,7 @@ export default function Page() {
                 <p className="mt-4"><a href="#contacto" className="inline-block px-6 py-3 rounded-full bg-white text-customBlue hover:bg-opacity-80 transition duration-300 ease-in-out no-underline font-semibold text-xl">Contáctanos</a></p>
             </div>
             <div className="flex justify-end">
-                <Image src={rockett} alt="Cohete 1" title="GIF de David Montero de Pixabay" className="hidden sm:block" width="400" />
+                <Image src={rockett} alt="Cohete 1" title="GIF de David Montero de Pixabay" className="hidden sm:block" width="610" />
             </div>
         </section>
         <section className="flex flex-col items-center justify-center bg-white py-8 sm:py-12 md:py-16 lg:py-20 -mt-16">
@@ -51,8 +51,8 @@ export default function Page() {
         <section className="bg-customBlue flex flex-col items-center sm:flex-row sm:justify-between sm:items-center -mt-8 sm:-mt-16 md:-mt-16 lg:-mt-16 h-610px md:h-843px overflow-hidden">
             <div className="flex flex-col items-start text-left p-12 sm:w-1/2">
                 <h2 className="font-semibold text-3xl leading-tight">Social Media</h2>
-                <p className="font-medium text-black text-sm sm:text-md md:text-lg lg:text-xl leading-normal mt-5">Nuestro servicio de social media mensual es tu solución integral para una presencia efectiva en línea. Nos encargamos de gestionar tus perfiles en redes sociales, creando contenido cautivador y relevante que conecte con tu audiencia.</p>
-                <p className="mt-5"><a href="#contacto" className="inline-block px-6 py-3 rounded-full bg-white text-customBlue hover:bg-opacity-80 transition duration-300 ease-in-out no-underline font-semibold text-base">Conoce más</a></p>
+                <p className="font-medium text-black text-sm sm:text-md md:text-lg lg:text-xl leading-normal mt-6">Nuestro servicio de social media mensual es tu solución integral para una presencia efectiva en línea. Nos encargamos de gestionar tus perfiles en redes sociales, creando contenido cautivador y relevante que conecte con tu audiencia.</p>
+                <p className="mt-8"><a href="#contacto" className="inline-block px-6 py-3 rounded-full bg-white text-customBlue hover:bg-opacity-80 transition duration-300 ease-in-out no-underline font-semibold text-base">Conoce más</a></p>
             </div>
             <div className="sm:w-1/2 sm:flex sm:justify-center sm:items-center h-full w-full overflow-hidden">
                 <Image src={foto1} alt="foto1" objectFit="cover" className="sm:block object-cover object-center h-full w-full overflow-hidden" /> 
@@ -64,15 +64,15 @@ export default function Page() {
             </div>
             <div className="flex flex-col items-start text-left p-12 sm:w-1/2">
                 <h2 className="font-semibold text-3xl leading-tight text-customBlue">Páginas Web</h2>
-                <p className="font-medium text-black text-sm sm:text-md md:text-lg lg:text-xl leading-normal mt-5">Nuestro servicio de diseño web ofrece soluciones personalizadas para crear una presencia en línea impactante y funcional para tu negocio. Desde sitios web informativos hasta tiendas en línea dinámicas, nuestro equipo experto trabaja contigo para entender tus necesidades y objetivos específicos.</p>
-                <p className="mt-5"><a href="#contacto" className="inline-block px-6 py-3 rounded-full bg-customBlue text-white hover:bg-opacity-80 transition duration-300 ease-in-out no-underline font-semibold text-base">Conoce más</a></p>
+                <p className="font-medium text-black text-sm sm:text-md md:text-lg lg:text-xl leading-normal mt-6">Nuestro servicio de diseño web ofrece soluciones personalizadas para crear una presencia en línea impactante y funcional para tu negocio. Desde sitios web informativos hasta tiendas en línea dinámicas, nuestro equipo experto trabaja contigo para entender tus necesidades y objetivos específicos.</p>
+                <p className="mt-8"><a href="#contacto" className="inline-block px-6 py-3 rounded-full bg-customBlue text-white hover:bg-opacity-80 transition duration-300 ease-in-out no-underline font-semibold text-base">Conoce más</a></p>
             </div>
         </section>
         <section className="bg-customBlue flex flex-col items-center sm:flex-row sm:justify-between sm:items-center -mt-8 sm:-mt-16 md:-mt-16 lg:-mt-16 h-610px md:h-843px overflow-hidden">
             <div className="flex flex-col items-start text-left p-12 sm:w-1/2">
                 <h2 className="font-semibold text-3xl leading-tight">Flyers a pedido</h2>
-                <p className="font-medium text-black text-sm sm:text-md md:text-lg lg:text-xl leading-normal mt-5">Nuestro servicio de flyers a pedido ofrece una solución ágil y personalizada para todas tus necesidades de promoción. Las medidas técnicas de los flyers son las siguientes:<br/><br/>-Papel Couche 170grs.<br/>-10cms de ancho x 14cms de alto<br/>-100, 500 o 1.000 unidades</p>
-                <p className="mt-5"><a href="#contacto" className="inline-block px-6 py-3 rounded-full bg-white text-customBlue hover:bg-opacity-80 transition duration-300 ease-in-out no-underline font-semibold text-base">Conoce más</a></p>
+                <p className="font-medium text-black text-sm sm:text-md md:text-lg lg:text-xl leading-normal mt-6">Nuestro servicio de flyers a pedido ofrece una solución ágil y personalizada para todas tus necesidades de promoción. Las medidas técnicas de los flyers son las siguientes:<br/><br/>-Papel Couche 170grs.<br/>-10cms de ancho x 14cms de alto<br/>-100, 500 o 1.000 unidades</p>
+                <p className="mt-8"><a href="#contacto" className="inline-block px-6 py-3 rounded-full bg-white text-customBlue hover:bg-opacity-80 transition duration-300 ease-in-out no-underline font-semibold text-base">Conoce más</a></p>
             </div>
             <div className="sm:w-1/2 sm:flex sm:justify-center sm:items-center h-full w-full overflow-hidden">
                 <Image src={foto3} alt="foto1" objectFit="cover" className="sm:block object-cover object-center h-full w-full overflow-hidden" /> 
@@ -84,8 +84,8 @@ export default function Page() {
             </div>
             <div className="flex flex-col items-start text-left p-12 sm:w-1/2">
                 <h2 className="font-semibold text-3xl leading-tight text-customBlue">Campañas masivas</h2>
-                <p className="font-medium text-black text-sm sm:text-md md:text-lg lg:text-xl leading-normal mt-5">Nuestro servicio de campañas masivas te ofrece una potente herramienta para llegar a una audiencia amplia y segmentada de manera efectiva. Desde correos electrónicos hasta mensajes de texto y anuncios en redes sociales, diseñamos estrategias integrales que maximizan el alcance y la participación de tu marca.</p>
-                <p className="mt-5"><a href="#contacto" className="inline-block px-6 py-3 rounded-full bg-customBlue text-whitee hover:bg-opacity-80 transition duration-300 ease-in-out no-underline font-semibold text-base">Conoce más</a></p>
+                <p className="font-medium text-black text-sm sm:text-md md:text-lg lg:text-xl leading-normal mt-6">Nuestro servicio de campañas masivas te ofrece una potente herramienta para llegar a una audiencia amplia y segmentada de manera efectiva. Desde correos electrónicos hasta mensajes de texto y anuncios en redes sociales, diseñamos estrategias integrales que maximizan el alcance y la participación de tu marca.</p>
+                <p className="mt-8"><a href="#contacto" className="inline-block px-6 py-3 rounded-full bg-customBlue text-whitee hover:bg-opacity-80 transition duration-300 ease-in-out no-underline font-semibold text-base">Conoce más</a></p>
             </div>
         </section>
 
