24bc099e4330bef883a0461f61257c8cd03e1192
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-06-19 00:41:04 -0400

    mucho commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,8 +3,7 @@ import { Header } from '../components/header';
 
 export const metadata = {
     title: {
-        template: '%s | Netlify',
-        default: 'Netlify Starter'
+        default: 'Total Medios'
     }
 };
 
