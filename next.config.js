b07cbabb7fbaae7d24bcc30961933ceb1dbe2867
Enrique Fuenzalida <114259706+enriquefuenzalida@users.noreply.github.com>
2024-06-19 09:56:08 -0400

    rocket dos

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true
+    swcMinify: true,
+    images: {
+        formats: ['image/webp'], // Disable AVIF support by removing 'image/avif'
+    },
 };
 
 module.exports = nextConfig;
